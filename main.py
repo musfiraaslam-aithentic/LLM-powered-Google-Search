4a7209142ffdd475c2c02a66237262677a2150a4
muhammadamer-dev <muhammad.amer@aithentic.com>
2025-11-25 18:45:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ app = FastAPI(
     )
 
 default_tech_groups = tools.get_tech_groups()
+default_tech_types = tools.get_tech_types()
+
 
 class ProcessRequest(BaseModel):
     metadata: Dict[str, Any]
     tech_groups: Optional[List[str]] = None
+    tech_types: Optional[List[str]] = None
 
 
 @app.get("/")
@@ -28,16 +31,22 @@ def health() -> dict:
 def process_asset_endpoint(request: ProcessRequest):
     metadata = request.metadata
     tech_groups = request.tech_groups
+    tech_types = request.tech_types
 
     if tech_groups and len(tech_groups) > 0:
         processed_tech_groups = tools.encode(tech_groups)
     else:
         processed_tech_groups = default_tech_groups
 
+    if tech_types and len(tech_types) > 0:
+        processed_tech_types = tools.encode(tech_types)
+    else:
+        processed_tech_types = default_tech_types
+
     print("Incoming metadata:", metadata)
 
     asset = {"metadata": metadata}
-    result = tools.process_asset(asset, processed_tech_groups)
+    result = tools.process_asset(asset, processed_tech_groups, processed_tech_types)
 
     # return {
     #     "status": "success",
